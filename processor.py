9fce863c56d6f5dfd3c97c77027f62d54bc24bb6
Jordan Olin <jolin@xmatters.com>
2018-12-17 11:27:13 -0700

    User and Device restore working

--- processor.py
+++ processor.py
@@ -21,27 +21,30 @@ import common_logger
 
 _logger = None
 _users = None
+_site_dict = {}
+_user_dict = {}
+_supervisor_dict = {}
+
 
 def _log_xm_error(url, response):
     """Captures and logs errors
         
-        Logs the error caused by attempting to call url.
+    Logs the error caused by attempting to call url.
         
-        Args:
+    Args:
         url (str): The location being requested that caused the error
         response (object): JSON object that holds the error response
-        """
+    """
     body = response.json()
     if response.status_code == 404:
         _logger.warn(config.ERR_INITIAL_REQUEST_FAILED_MSG,
                      response.status_code, url)
     else:
-        _logger.error(config.ERR_INITIAL_REQUEST_FAILED_MSG,
-                      response.status_code, url)
-        _logger.error('Response - code: %s, reason: %s, message: %s',
-                    str(body['code']) if 'code' in body else "none",
-                    str(body['reason']) if 'reason' in body else "none",
-                    str(body['message']) if 'message' in body else "none")
+        _logger.error(f'Response - ' \
+                      f'code: {body["code"] if "code" in body else "none"}, ' \
+                      f'reason: {body["reason"] if "reason" in body else "none"}, ' \
+                      f'message: {body["message"] if "message" in body else "none"}, ' \
+                      f'\n\tURL: {url}')
 
 def _open_in_file(filename: str) -> TextIOBase:
     """Opens data file to restore
@@ -58,17 +61,17 @@ def _open_in_file(filename: str) -> TextIOBase:
 def _add_site(site_json):
     """Attempst to add a new Site object from the JSON string.
         
-        Creates a dict object to pass to xMatters to create a new site
+    Creates a dict object to pass to xMatters to create a new Site
         
-        Args:
-        site_json: The JSON payload representing the site to add
+    Args:
+        site_json: The JSON payload representing the Site to add
     """
-    # C
+    # Conver the Site JSON string to an object
     site_obj = json.loads(site_json)
     
     # Set our resource URLs
     url = config.xmod_url + '/api/xm/1/sites'
-    _logger.debug('Attempting to create site with body:\n\t "%s"\n\tvia url: %s', site_json, url)
+    _logger.debug('Attempting to create Site with body:\n\t "%s"\n\tvia url: %s', site_json, url)
 
     # Initialize loop with first request
     try:
@@ -113,29 +116,301 @@ def _process_sites():
                 site_json = site_json[:-2] if site_json[-2:] == ',\n' else site_json[:-1]
                 num_lines += 1
                 site_obj = _add_site(site_json)
+                _site_dict[site_obj['name']] = site_obj
                 num_sites += 0 if site_obj == None else 1
 
     _logger.debug("Restored %d of a possible %d Sites.", num_sites, num_lines)
 
-def _process_users():
+def _get_site(name: str):
+    """Get a site object by name
+
+    Retrieves the Site object record from xMatters based on it's name.
+
+    Args:
+        name (str): Site name to retrieve
+
+    Return:
+        site (dict): The found site object
+    """
+    if name in _site_dict:
+        _logger.debug('Found Site "%s"', name)
+        return _site_dict[name]
+
+    # Initialize conditions
+    url = config.xmod_url + '/api/xm/1/sites/' + urllib.parse.quote(name)
+    _logger.debug('Retrieving Site, url=%s', url)
+
+    # Get the site records
+    response = requests.get(url, auth=config.basic_auth)
+    if response.status_code not in [200]:
+        _log_xm_error(url, response)
+        return None
+
+    # Process the responses
+    site = response.json()
+    _site_dict[name] = site
+    _logger.debug('Retrieved Site "%s"', name)
+
+    return site
+
+def _add_devices(user_id: str, target_name: str, devices: list):
+    """Attempst to add the Device objects from the Device list.
+        
+    Creates Device objects based on the device_list
+    
+    Args:
+        user_id (str): The UUID of the User to add the devices to
+        target_name (str): The targetName field for the User to add devices to
+        devices (list): The list object containing the Devices to add
+    """
+    dev_count = 0
+    for device in devices:
+
+        # Update the device
+        device['owner'] = user_id
+        del device['targetName']
+        del device['links']
+        if 'timeframes' in device:
+            if device['timeframes']['total'] > 0 and 'data' in device['timeframes']:
+                device['timeframes'] = device['timeframes']['data']
+            else:
+                del device['timeframes']
+
+        # Set our resource URLs
+        url = config.xmod_url + '/api/xm/1/devices'
+        _logger.debug('Attempting to create Device "%s" for User Id "%s"\n\tvia url: %s\n\twith payload: %s', device['name'], user_id, url, json.dumps(device))
+
+        # Initialize loop with first request
+        try:
+            response = requests.post(url,
+                                    headers = {'Content-Type': 'application/json'},
+                                    data = json.dumps(device),
+                                    auth=config.basic_auth)
+        except requests.exceptions.RequestException as e:
+            _logger.error(config.ERR_REQUEST_EXCEPTION_CODE, url, repr(e))
+            continue
+
+        # If the initial response fails, log and return null
+        if response.status_code not in [200, 201]:
+            _log_xm_error(url, response)
+            continue
+
+        # Process the response
+        user_device = response.json()
+        _logger.info(f'Created/Updated Device "{target_name}|{user_device["name"]}" - Id: {user_device["id"]}')
+        dev_count += 1
+        # _logger.debug('Created/Updated Device "%s" - json body: %s', user_device['name'], pprint.pformat(user_device))
+
+    _logger.debug(f'Added {dev_count} of a possible ' \
+                f'{len(devices)} devices for ' \
+                f'{target_name}')
+
+    return dev_count
+
+def _add_user(include_devices: bool, user_json: str):
+    """Attempst to add a new User object from the JSON string.
+        
+    Creates a dict object to pass to xMatters to create a new User
+    
+    Args:
+        include_devices (bool): If True, restore the User's devices too
+        user_json (str): The JSON payload representing the User to add
+    """
+    # Convert the User JSON string to an object
+    full_user_obj = json.loads(user_json)
+    user_obj = full_user_obj['user']
+
+    # Prepare the object for adding back in
+    del user_obj['links']
+    site = _get_site(user_obj['site']['name'])
+    del user_obj['site']
+    user_obj['site'] = site['id']
+    role_list = []
+    is_comp_admin = False
+    for role in user_obj['roles']['data']:
+        if role['name'] == 'Company Admin': is_comp_admin = True
+        role_list.append(role['name'])
+    # If a Company Admin, return as there is nothing to do
+    if is_comp_admin: 
+        _logger.warn('Unable to add internal xMatters User with Role "Company Admin": %s', 
+            user_obj['firstName'] + ' ' + user_obj['lastName'] + ' (' + user_obj['targetName'] + ')')
+        return
+
+    # Continue user preprocessing
+    del user_obj['roles']
+    user_obj['roles'] = role_list
+
+    # Build supervisors list to preserve after we get ID
+    supervisors = []
+    if 'supervisors' in user_obj:
+        if user_obj['supervisors']['total'] > 0:
+            for supervisor in user_obj['supervisors']['data']:
+                supervisors.append(supervisor['targetName'])
+        del user_obj['supervisors']
+    user_obj['supervisors'] = []
+            
+    # Set our resource URLs
+    url = config.xmod_url + '/api/xm/1/people'
+    _logger.debug('Attempting to create User with body:\n\t "%s"\n\tvia url: %s', json.dumps(user_obj), url)
+
+    # Initialize loop with first request
+    try:
+        response = requests.post(url,
+                                 headers = {'Content-Type': 'application/json'},
+                                 data = json.dumps(user_obj),
+                                 auth=config.basic_auth)
+    except requests.exceptions.RequestException as e:
+        _logger.error(config.ERR_REQUEST_EXCEPTION_CODE, url, repr(e))
+        return None
+
+    # If the initial response fails, log and return null
+    if response.status_code not in [200, 201]:
+        _log_xm_error(url, response)
+        return None
+
+    # Process the response
+    new_user_obj = response.json()
+    _user_dict[new_user_obj['targetName']] = new_user_obj['id']
+    _supervisor_dict[new_user_obj['id']] = supervisors
+    _logger.info('Created/Updated User "%s" - Id: %s', new_user_obj['targetName'], new_user_obj['id'])
+    # _logger.debug('Created/Updated User "%s" - json body: %s', new_user_obj['targetName'], pprint.pformat(new_user_obj))
+
+    # If we need to add devices, do that now
+    if include_devices:
+        dev_count = _add_devices(new_user_obj['id'], new_user_obj['targetName'], full_user_obj['devices'])
+
+    return user_obj
+
+def _get_user(targetName: str):
+    """Get a User's id by targetName
+
+    Retrieves the User object record from xMatters based on it's targetName.
+
+    Args:
+        name (str): Target Name to retrieve
+
+    Return:
+        user_id (str): The found User ID
+    """
+    if targetName in _user_dict:
+        user_id = _user_dict[targetName]
+        _logger.debug('Found ID "%s" for User "%s"', user_id, targetName)
+        return user_id
+
+    # Initialize conditions
+    url = config.xmod_url + '/api/xm/1/people/' + urllib.parse.quote(targetName)
+    _logger.debug('Retrieving User, url=%s', url)
+
+    # Get the site records
+    response = requests.get(url, auth=config.basic_auth)
+    if response.status_code not in [200]:
+        _log_xm_error(url, response)
+        _user_dict[targetName] = None
+        return None
+
+    # Process the responses
+    user = response.json()
+    _user_dict[user['targetName']] = user['id']
+    _logger.debug('Retrieved User "%s"', user['targetName'])
+
+    return user['id']
+
+def _add_user_supervisors(user_id: str, target_name: str):
+    """Attempts to update the new User object with supervisors.
+        
+    Updates Creates a dict object to pass to xMatters to create a new User
+        
+    Args:
+        user_id (str): The User's Id to add supervisors
+        target_name (str): The User's targetName to add supervisors
+    """
+    # Is there anything to do
+    if not user_id in _supervisor_dict or len(_supervisor_dict[user_id]) == 0:
+        _logger.debug('No supervisors for user_id: %s, target_name: %s', user_id, target_name) 
+        return 0
+
+    # Setup the update object
+    user = {}
+    user['id'] = user_id
+    user['targetName'] = target_name
+    supervisors = []
+    for targetName in _supervisor_dict[user_id]:
+        super_id = _get_user(targetName)
+        if super_id:
+            supervisors.append(super_id)
+        else:
+            _logger.warn('Unable to find Supervisor (%s) for User (%s).', targetName, target_name)
+    if len(supervisors) == 0:
+        _logger.debug('No valid supervisors for user_id: %s, target_name: %s', user_id, target_name) 
+        return 0
+    user['supervisors'] = supervisors
+
+    # Set our resource URLs
+    url = config.xmod_url + '/api/xm/1/people'
+    _logger.debug('Attempting to update the Supervisors for User:\n\t "%s"\n\tvia url: %s', json.dumps(user), url)
+
+    # Initialize loop with first request
+    try:
+        response = requests.post(url,
+                                 headers = {'Content-Type': 'application/json'},
+                                 data = json.dumps(user),
+                                 auth=config.basic_auth)
+    except requests.exceptions.RequestException as e:
+        _logger.error(config.ERR_REQUEST_EXCEPTION_CODE, url, repr(e))
+        return 0
+
+    # If the initial response fails, log and return null
+    if response.status_code not in [200, 201]:
+        _log_xm_error(url, response)
+        return 0
+
+    # Process the response
+    upd_user_obj = response.json()
+    _logger.info('Updated Supervisors for User "%s" - Id: %s', upd_user_obj['targetName'], upd_user_obj['id'])
+    # _logger.debug('Created/Updated User "%s" - json body: %s', upd_user_obj['targetName'], pprint.pformat(upd_user_obj))
+    return 1
+
+def _process_users(include_devices: bool):
     """Reads and restored the instances User objects
 
     Retrieves the Users object records from the file system and adds/updates
     them into the target xMatters instance.
 
     Args:
-        None
+        include_devices (bool): If True, restore the User's devices too
 
     Return:
         None
     """
-    pass
+    # First add the users without supervisors
+    with open(config.users_filename) as users_file:
+        num_lines = 0
+        num_users = 0
+        for user_json in users_file:
+            # Ignore the opening array markers ("[\n" and "]\n")
+            if len(user_json) > 2:
+                # Remove trailing ",\n" or trailing "\n"
+                user_json = user_json[:-2] if user_json[-2:] == ',\n' else user_json[:-1]
+                num_lines += 1
+                user_obj = _add_user(include_devices, user_json)
+                if user_obj:
+                    _user_dict[user_obj['targetName']] = user_obj['id']
+                    num_users += 0 if user_obj == None else 1
+
+    _logger.debug("Restored %d of a possible %d Users.", num_users, num_lines)
+
+    # Next add the users supervisors
+    num_users = 0
+    user_dict = {x:y for x,y in _user_dict.items()}
+    for target_name, user_id in user_dict.items():
+        num_users += _add_user_supervisors(user_id, target_name)
+    _logger.debug("Updated supervisors for %d of a possible %d Users.", num_users, len(_user_dict))
 
 def _process_devices():
     """Reads and restored the instances User's Device objects
 
-    Retrieves the User's Device object records from the file system and adds/updates
-    them into the target xMatters instance.
+    Retrieves the Users object records from the file system and adds/updates
+    the associated Devices into the target xMatters instance.
 
     Args:
         None
@@ -143,7 +418,28 @@ def _process_devices():
     Return:
         None
     """
-    pass
+    _logger.info('Processing Devices independent of Users.')
+    # Go through the Users file and pull out the device info
+    with open(config.users_filename) as users_file:
+        max_devices = 0
+        num_devices = 0
+        num_lines = 0
+        for user_json in users_file:
+            # Ignore the opening array markers ("[\n" and "]\n")
+            if len(user_json) > 2:
+                # Remove trailing ",\n" or trailing "\n"
+                user_json = user_json[:-2] if user_json[-2:] == ',\n' else user_json[:-1]
+                num_lines += 1
+                full_user_obj = json.loads(user_json)
+                user_obj = full_user_obj['user']
+                max_devices += len(full_user_obj['devices'])
+                # Try to get the "id" from the user dictionary, otherwise
+                # retrieve "id" field directly from xMatters as it may have
+                # changed upon recovery
+                user_id = _get_user(user_obj['targetName'])
+                num_devices += _add_devices(user_id, user_obj['targetName'], full_user_obj['devices'])
+
+    _logger.info(f"Restored {num_devices} of a possible {max_devices} Devices from {num_lines} Users.")
 
 def _process_groups():
     """Reads and restored the instances Groups objects
@@ -175,19 +471,17 @@ def process(objects_to_process: list):
     ### Get the current logger
     _logger = common_logger.get_logger()
 
-    # Capture and save the Site objects
+    # Read and restore the Site objects
     if 'sites' in objects_to_process:
         _process_sites()
 
-    # Capture and save the User objects
+    # Read and restore the User objects, and possibly devices
     if 'users' in objects_to_process:
-        _process_users()
-
-    # Capture and save the Device objects
-    if 'devices' in objects_to_process:
+        _process_users('devices' in objects_to_process)
+    elif 'devices' in objects_to_process:
         _process_devices()
 
-    # Capture and save the Device objects
+    # Read and restore the Group objects
     if 'groups' in objects_to_process:
         _process_groups()
 
